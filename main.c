6b742fdd945ef054d7e19e1f15e73f39cf3ec9f5
Rafael Verduzco <rverduzco@gmail.com>
2013-05-11 18:22:17 -0500

    Changed initial conditions

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, const char * argv [])
 	const int nodes = 10;
 	const int max_iterations = 1000;
 	const int transient = 50;
-	int initial_condition[10] = {1,0,1,0,1,0,1,0,1,0};
+	int initial_condition[10] = {0,0,0,1,0,0,0,1,0,0};
 	const char * names = "Sr\tcGMP\tPDE\tV\tCatsper\tHCN\tpH\tdCa\tCaKC\tdK";
 	FILE * fp = fopen("salida.txt", "w+");
 	discrete_evolution(initial_condition, nodes, max_iterations, transient, names, discrete_model, fp);
