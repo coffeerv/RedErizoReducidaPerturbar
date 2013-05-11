893c44984b5b6208b1f848e18c27617f9b3f3024
Rafael Verduzco <rverduzco@gmail.com>
2013-05-11 18:39:14 -0500

    Added a ridiculous message
    Actually, I'm testing a Jenkins installation =)

--- main.c
+++ main.c
@@ -13,5 +13,6 @@ int main(int argc, const char * argv [])
 	FILE * fp = fopen("salida.txt", "w+");
 	discrete_evolution(initial_condition, nodes, max_iterations, transient, names, discrete_model, fp);
 	fclose(fp);
+	printf("\nYa estufas!\n");
     	return EXIT_SUCCESS;
 }
